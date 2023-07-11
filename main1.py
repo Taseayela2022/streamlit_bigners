ba7e0017d000b5f3c464fef3eb09f72262fbe31d
Tase <119581380+Taseayela2022@users.noreply.github.com>
2023-07-11 19:55:05 +0100

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,42 @@
+import streamlit as st
+st.markdown("# Main page 🎈")
+x="WElcome all those interested to learn Python, Rstudio, Machine learning"
+st.sidebar.markdown(x)
+x = st.slider('x')  # 👈 this is a widget
+st.write(x, "Select your best contast level" ,x * x)
+st.title("welcome to my streamlit tought")
+st.write("   what is you name ")
+for x in range(100):
+	pass 
+	x=x+40
+st.write("your_age is \n")
+st.write(x)
+st.subheader("My 2023 Objectives")
+st.write(" What are them?")
+
+import pandas as pd
+df = pd.DataFrame({
+   
+  'ID': [1, 2, 3, 4],
+  'name of cl':["abe","kabe","alem", "Andu"],
+  'Age': [10, 20, 30, 40],
+  'Acyear':[2020, 2022, 2021, 2023],
+  'CGPA':[3.6, 4.0, 3.6, 3.8]
+})
+df
+option = st.selectbox(
+    'Which column data do you fit for your_info select? \n Age',
+
+     df['Age'])
+
+'you selected Age:', option
+option = st.selectbox(
+	'ID',
+     df['ID'])
+
+'you selected GPA:', option
+option = st.selectbox(
+    'cgpa',
+     df['CGPA'])
+
+'you selected:', option
